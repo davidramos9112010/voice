64261164d05bc81c37bd2b13141d4eca44378988
davidramos9112010 <81586209+davidramos9112010@users.noreply.github.com>
2021-09-14 21:58:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+var SpeechRecognition=window.webkitSpeechRecognition;
+var recognition=new  SpeechRecognition();
+function start()
+{
+document.getElementById("textbox").innerHTML="";
+recognition.start();
+
+}
+recognition.onresult=function(event)
+{console.log(event);
+ var Content=event.results[0][0].transcript;
+ document.getElementById("textbox").innerHTML = Content;
+  console.log(Content);
+  speak();
+}
+function speak()
+{
+var synth=window.speechSynthesis;
+speek_data=document.getElementById("textbox").value;
+var utterThis=new SpeechSynthesisUtterance(speek_data);
+synth.speek(utterThis);
+Webcam.attach(camera);
+
+
+}
+camera=document.getElementById("camera");
+Webcam.set({
+width:360,height:250,image_format:'png',png_quality:90
+
+});
+
+
+
+
+
